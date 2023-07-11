4fa7702c2edaf4015ae48d1f4319ce384e4be62a
shrfrn <shrfrn@gmail.com>
2023-07-11 17:40:43 +0300

    Add count computed prop. to footer = shared state

--- cmps/AppFooter.js
+++ cmps/AppFooter.js
@@ -2,11 +2,13 @@ export default {
     template: `
         <footer>
             <h4>Cart Total: \${{ totalPrice }}</h4>
+            <h4>Count: {{ count }}</h4>
         </footer>
     `,
     computed: {
         totalPrice() {
             return 0
-        }
+        },
+        count() { return this.$store.state.count }
     },
 }
